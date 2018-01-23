48516389d4d2f09c331cbe493dd153186d796e01
shishaofei <278045742@qq.com>
2018-01-23 17:38:31 +0800

    update

--- src/components/bookrack/bookrack.vue
+++ src/components/bookrack/bookrack.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
-    <m-header :data="headerText" :left-arrow="true" v-on:back-page="backPage">
-      <a class="sure-btn"><span>完成</span></a>
+    <m-header :data="headerText" :left-arrow="true">
+      <a @click="sureBtn" class="sure-btn"><span>完成</span></a>
     </m-header>
     <div class="list">
       <label v-for="item in bookListArr" class="list-item">
-        <input class="aui-checkbox" type="checkbox" name="checkbox">
+        <input :value="item.bookUri" :title="item.bookTitle" v-info="selectBook" class="aui-checkbox" type="checkbox" name="checkbox">
         <img class="item-img" v-lazy="'http://bookmark.xftimes.com'+item.photoCoverImage" alt="">
         <span class="item-text">{{item.bookTitle}}</span>
       </label>
@@ -21,23 +21,31 @@
     data() {
       return {
         headerText: '我的书架',
-        bookListArr: []
+        bookListArr: [],
+        selectBook: []
       }
     },
     created() {
       this._getBookList()
     },
     methods: {
+      // 点击完成所看的书
+      sureBtn() {
+        console.log(this.selectBook)
+      },
       // 获取书列表的接口
       _getBookList() {
         getBookList().then((res) => {
+          console.log(res)
           this.bookListArr = res.bookList
         })
-      },
-
-      // 返回上一页面，后面有待优化
-      backPage() {
-        this.$router.go(-1)
+      }
+    },
+    directives: {
+      info: {
+        bind (el) {
+          console.log(el.title)
+        }
       }
     },
     components: {
